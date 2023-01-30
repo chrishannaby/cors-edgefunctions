1abb5debd145b95b9f8ddd551905a6aedd84beec
Chris Hannaby <chris.hannaby@netlify.com>
2023-01-30 01:44:25 -0700

    make the API slow

--- netlify/edge-functions/recommended-collection.ts
+++ netlify/edge-functions/recommended-collection.ts
@@ -4,5 +4,8 @@ export default async (request: Request, context: Context) => {
   const options = ["men", "women"];
   const collection = Math.random() < 0.5 ? options[0] : options[1];
 
+  //wait for 2 seconds
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
   return context.json({ collection });
 };
